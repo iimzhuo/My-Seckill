03213e60a99ff583dc5a124068b8c103a52f0aa4
iimzhuo <1822638890@qq.com>
2021-06-10 11:58:33 +0800

    'create'

---
+++ seckill-consumer/src/test/java/com/re/seckillConsumer/SeckillConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.re.seckillConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
