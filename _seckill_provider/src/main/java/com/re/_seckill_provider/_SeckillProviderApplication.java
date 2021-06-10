7c610c7d04a6bc6de2c70802d5a4c7199d1055af
iimzhuo <1822638890@qq.com>
2021-06-10 18:00:35 +0800

    're'

--- _seckill_provider/src/main/java/com/re/_seckill_provider/_SeckillProviderApplication.java
+++ _seckill_provider/src/main/java/com/re/_seckill_provider/_SeckillProviderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SeckillProviderApplication {
+public class _SeckillProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SeckillProviderApplication.class, args);
+        SpringApplication.run(_SeckillProviderApplication.class, args);
     }
 
 }
