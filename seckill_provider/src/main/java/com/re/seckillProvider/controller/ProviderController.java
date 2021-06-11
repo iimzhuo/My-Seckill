c1e662e00ba8e2656cb4957cac2b24cb925842b5
iimzhuo <1822638890@qq.com>
2021-06-11 10:47:06 +0800

    add Swagger

---
+++ seckill_provider/src/main/java/com/re/seckillProvider/controller/ProviderController.java
@@ -0,0 +1,25 @@
+package com.re.seckillProvider.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/provider")
+@Api(tags = "Seckill 服务提供者")
+public class ProviderController {
+
+    @GetMapping
+    @ApiOperation("Seckill 秒杀接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id",value ="秒杀id",defaultValue = "1000",required = true)
+    })
+    public String Seckill(String id){
+        System.out.println(id);
+        return id;
+    }
+}
