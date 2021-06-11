b923c8af42a35b6df3a40e93d537ac3b360bd019
iimzhuo <1822638890@qq.com>
2021-06-11 15:04:53 +0800

    create consumer

---
+++ _seckill_provider/src/main/java/com/re/_seckill_provider/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.re._seckill_provider.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+//http://127.0.0.1:9091/swagger-ui.html
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.re._seckill_provider.controller"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfoBuilder()
+                        .title("Swagger-UI")
+                        .description("Seckill 服务提供者")
+                        .version("9.0")
+                        .build());
+    }
+}
--- seckill-consumer/src/main/java/com/re/seckillConsumer/controller/SeckillConsumer.java
+++ seckill-consumer/src/main/java/com/re/seckillConsumer/controller/SeckillConsumer.java
@@ -1,10 +1,29 @@
 package com.re.seckillConsumer.controller;
 
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/Seckill")
+@RequestMapping("/Seckill-Consumer")
+@Api(tags = "Seckill-Consumer 服务消费者")
 public class SeckillConsumer {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping
+    public String getProduct(){
+        ServiceInstance serviceInstance = loadBalancerClient.choose("seckill_provider");
+        String url = String.format("http://%s:%s/%s", serviceInstance.getHost(), serviceInstance.getPort(), "provider");
+        return restTemplate.getForObject(url, String.class);
+    }
 }
