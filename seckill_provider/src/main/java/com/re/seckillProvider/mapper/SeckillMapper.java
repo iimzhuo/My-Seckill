28c3e45f2006b92fe71ddf203673a59c08577ee9
iimzhuo <1822638890@qq.com>
2021-06-16 11:12:17 +0800

    添加了对于Mybatis-plus的支持

---
+++ seckill_provider/src/main/java/com/re/seckillProvider/mapper/SeckillMapper.java
@@ -0,0 +1,9 @@
+package com.re.seckillProvider.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.re.seckillProvider.entity.Seckill;
+
+public interface SeckillMapper extends BaseMapper<Seckill> {
+
+
+}
